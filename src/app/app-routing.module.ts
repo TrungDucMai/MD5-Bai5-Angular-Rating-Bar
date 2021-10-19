4008e542e026f75223be1c4802a027d8b0c5242a
TrungDucMai <trungmaiduc196@gmail.com>
2021-10-19 15:23:16 +0700

    r

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CountdownTimerComponent} from "./countdown-timer/countdown-timer.component";
+import {RatingBarComponent} from "./rating-bar/rating-bar.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"rating-bar", component: RatingBarComponent},
+  {path:"countdown-timer", component: CountdownTimerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
